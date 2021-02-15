c2f0bc65d86fcbde6a20393cca7f0443b0a86b8f
Jordan Renaud <jordan.dj.renaud@gmail.com>
2021-02-15 10:29:00 +0000

    add suport for more emails

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
 function checkEmail() {
     const emailInput = document.getElementById("email-input").value;
-    console.log(emailInput)
+
+    let regex = /[\s\S]*@gmail\.com|@live\.com|@yahoo\.co\.uk/
 
     if (emailInput == "") {
         alert("Please enter email");
-    } else if (emailInput.endsWith("gmail.com")){
+    } else if ((regex.test(emailInput))){
         alert("Success")
     } else {
         alert("failure")
     }
+    
     
 }
 
