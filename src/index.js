d9fa5cf293847ed909d073689a1679597d707e3e
Jordan Renaud <jordan.dj.renaud@gmail.com>
2021-02-22 10:17:32 +0000

    trigger on enter key

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 function checkEmail() {
     const emailInput = document.getElementById("email-input").value;
-
-    let regex = /[\s\S]*@gmail\.com|@live\.com|@yahoo\.co\.uk/
+    let regex = /[\s\S]+@gmail\.com|[\s\S]+@live\.com|[\s\S]+@yahoo\.co\.uk/;
 
     if (emailInput == "") {
         alert("Please enter email");
@@ -9,12 +8,18 @@ function checkEmail() {
         alert("Success")
     } else {
         alert("failure")
-    }
-    
-    
+    } 
 }
 
+//button functionality
 const goButton = document.getElementById("go-button");
 goButton.addEventListener("click", checkEmail)
 
 let emailInput = document.getElementById("email-input");
+emailInput.addEventListener("keyup", function(event) {
+    if (event.keyCode === 13) {
+        goButton.click();
+
+    }
+})
+
